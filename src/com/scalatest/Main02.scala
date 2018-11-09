373a70a0fcc3eebac5988da5f447d509d23a352b
一个有素质的人 <36368282+ZMDProgrom@users.noreply.github.com>
2018-11-09 19:08:58 +0800

    试一试

---
+++ src/com/scalatest/Main02.scala
@@ -0,0 +1,6 @@
+package com.scalatest
+
+//scala入口函数方式二
+object Main02 extends  App{
+  println("extends scala1")
+}
---
+++ src/com/scalatest/OBScala.scala
@@ -0,0 +1,29 @@
+package com.scalatest
+
+object OBScala {
+  //入口函数编写方式一
+  def main(args: Array[String]): Unit = {
+    println("hello,scala")
+
+    //定义变量的两种方式
+
+    //第一种方式:val修饰的变量
+    //特点:值不可以修改,相当于java里面的final修饰的变量
+
+    val x=10
+    //x=20
+    println(x)
+
+    //第二种方式:var修饰的变量
+    //特点:值是可以修改的
+    var y=20
+    y = 300
+    println(y)
+
+    //显式声明变量类型
+    //变量名在前,类型在后
+    val a: Int = 100
+    val b: String = "xxxlll"
+    println(a+b)
+  }
+}
